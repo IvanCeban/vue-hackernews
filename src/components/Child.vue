1cdecde3c10afc8886eb841513f3ac2610067561
IvanCeban <ivan.ceban@gmail.com>
2019-05-14 13:33:56 +0300

    Chapter 3 Testing classes, inline styles, children rendering and props

---
+++ src/components/Child.vue
@@ -0,0 +1,10 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "Child",
+        props: ['testProp']
+    }
+</script>
---
+++ src/components/TestComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <child v-for="item in items" :key="item.id" testProp="some-value"/>
+      <a href="https://google.com">Link</a>
+      <p :style="{'color': 'red'}">Some text</p>
+    </div>
+</template>
+
+<script>
+  import Child from './Child.vue'
+  export default {
+    name: "TestComponent",
+    components: { Child },
+    data () {
+      return {
+        items: window.items
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
